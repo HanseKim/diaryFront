1862e25da5b71897e4c09854fcc3028c9b2b4604
HanseKim <kimbird2002@naver.com>
2025-02-24 00:42:57 +0900

    .

---
+++ CoupleInfoComponent.tsx
@@ -0,0 +1,163 @@
+import React from "react";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Alert,
+} from 'react-native';
+import { apiClient } from '../utils/apiClient';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+type CoupleInfoComponentProps = {
+  name : string
+  daysPassed : number
+  diarycount : { [key: number]: number }
+  couple_month : number
+  couple_all : number
+  onDeleteSuccess: () => void 
+}
+const CoupleInfoComponent:React.FC<CoupleInfoComponentProps> = ({name,daysPassed,diarycount, couple_month, couple_all, onDeleteSuccess }) => {
+  const handleDeleteCouple = () => {
+    Alert.alert(
+      "커플 삭제",
+      "채팅도 삭제됩니다. 정말 커플 관계를 삭제하시겠습니까?",
+      [
+        {
+          text: "취소",
+          style: "cancel"
+        },
+        {
+          text: "삭제",
+          style: "destructive",  // iOS에서 빨간색으로 표시됩니다
+          onPress: async () => {
+            try {
+              const response = await apiClient.post("/mypage/delete-couple");
+              await AsyncStorage.removeItem("groupid");
+              await AsyncStorage.removeItem("chatdata");
+              if (response.data.success) {
+                Alert.alert("성공", "커플 관계가 삭제되었습니다.");
+                onDeleteSuccess(); 
+                // 성공 후 필요한 상태 업데이트나 화면 새로고침 로직 추가
+              } else {
+                Alert.alert("오류", "커플 삭제에 실패했습니다.");
+              }
+            } catch (error) {
+              console.error("Error deleting couple:", error);
+              Alert.alert("오류", "서버 오류가 발생했습니다.");
+            }
+          }
+        }
+      ]
+    );
+  };
+  
+  const mood = ['sad','angry','normal','smile','happy'];
+  const moodImages: { [key: string]: any } = {
+    sad: require("../images/CoupleFace/sad.png"),
+    angry: require("../images/CoupleFace/angry.png"),
+    normal: require("../images/CoupleFace/normal.png"),
+    smile: require("../images/CoupleFace/smile.png"),
+    happy: require("../images/CoupleFace/happy.png"),
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.title}>
+        <View style={{height: 40, justifyContent: 'center'}}>
+          <Text style={{
+            fontSize: 25,
+            fontWeight: 'bold',
+          }}>{name}</Text>
+          {/* <Text style={{
+            fontSize: 15, 
+          }}>{daysPassed!==-1 ? `D+${daysPassed}` : "D+???"}</Text> */}
+        </View>
+        <View style={{height:30}}>
+          <TouchableOpacity onPress={handleDeleteCouple}>
+            <Text style={{
+              paddingTop: 10,
+              color: '#FF5C85'
+            }}>커플 삭제하기</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.contents}>
+          <Text style={styles.contentText}>한달동안 쓴 일기 : {couple_month}개</Text>
+          <Text style={styles.contentText}>총 일기 : {couple_all}개</Text>
+      </View>
+      <View style={styles.moodcontainer}>
+        <Text style={styles.contentText}>한달동안의 기분</Text>
+        <View style={styles.imglist}>
+          {mood.map((item, index) => (
+            <View style={styles.facecontainer} key={item}>
+              <Image source={moodImages[item]} style={styles.icons} />
+              <Text>{diarycount[index + 1] || 0}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container : {
+    width: 350,
+    height: 300,
+    marginBottom: 5,
+    padding: 10,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    shadowColor: '#FF5C85',
+    shadowOffset: {
+      width: 8,
+      height: 8,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+    elevation: 8,
+  },
+  title : {
+    flex: 0.3,
+    height: 40,
+    margin: 10,
+    marginBottom: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  contents : {
+    margin: 10,
+    marginTop: 30,
+  },
+  contentText : {
+    marginBottom: 10,
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  moodcontainer : {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  facecontainer:{
+    marginRight: 5,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imglist:{
+    width: '100%',
+    height: 70,
+    flexDirection: 'row',
+    justifyContent: "space-evenly"
+  },
+  icons : {
+    width: 40,
+    height: 40,
+    resizeMode : "center",
+    marginBottom: 5,
+  }
+})
+
+export default CoupleInfoComponent;
---
+++ EditDiaryScreen.tsx
@@ -0,0 +1,462 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from 'react-native';
+import axios from 'axios';
+import { apiClient } from '../utils/apiClient';
+
+const EditDiaryScreen: React.FC<{ route: any; navigation: any }> = ({ route, navigation }) => {
+  const { diaryId } = route.params; // 이전 화면에서 전달받은 diaryId
+  const [date, setDate] = useState(new Date());
+  const [day, setDay] = useState(''); // "****-**-** "
+  const [headline, setHeadline] = useState('');
+  const [content, setContent] = useState('');
+  const [user_id, setUser_id] = useState('');
+  const [mood, setMood] = useState<string | null>(null);
+  const [privacy, setPrivacy] = useState<'Private' | 'Couple' | null>('Private');
+
+  // 백엔드에서 일기 데이터 가져오기
+  const fetchDiaryData = async () => {
+    try {
+        console.log(diaryId);
+        const response = await apiClient.post(`/diary/edit-search`, {
+          id: diaryId,
+        });
+      if(response.data){
+        setDate(new Date(response.data.diary_date));
+        setDay(response.data.diary_date);
+        setHeadline(response.data.title);
+        setContent(response.data.content);
+        setUser_id(response.data.user_id);
+        setMood(response.data.feeling ? ['😞', '😠', '😐', '😊', '😄'][response.data.feeling - 1] : null);
+        setPrivacy(response.data.privacy); // "Private" or "Couple"
+        console.log("Data: ",response.data)
+      } else {
+        console.error('Error fetching diary data:', response.data);
+      }
+    } catch (error) {
+      console.error('Error fetching diary data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDiaryData(); // 화면이 로드될 때 데이터 가져오기
+  }, []);
+
+  const formatDate = (date: Date) => {
+    return `${date.getMonth() + 1}/${date.getDate()}`;
+  };
+
+  const handlePrevDate = () => {
+    const prevDate = new Date(date);
+    prevDate.setDate(date.getDate() - 1);
+    setDate(prevDate);
+  };
+
+  const handleNextDate = () => {
+    const currentDate = new Date();
+    const nextDate = new Date(date);
+    nextDate.setDate(date.getDate() + 1);
+
+    if (nextDate <= currentDate) {
+      setDate(nextDate);
+    }
+  };
+
+  const getMoodIndex = (emoji: string | null) => {
+    const moods = ['😞', '😠', '😐', '😊', '😄'];
+    return emoji ? moods.indexOf(emoji) + 1 : 0;
+  };
+
+  const handleSave = async () => {
+    const diaryData = {
+      title: headline,
+      id: diaryId,
+      user_id: user_id,
+      content: content,
+      feeling: getMoodIndex(mood),
+      privacy: privacy,
+      diary_date: day,
+    };
+    console.log("diaryData : ", diaryData);
+  
+    try {
+      const response = await apiClient.post('/diary/edit-diary', diaryData);
+  
+      console.log("API Response:", response); // 추가된 로그
+      const data = response.data; // response.data를 직접 가져오기
+  
+      if (data) {
+        navigation.navigate('Detail', {
+          clickdate: date.getDate(),
+          clickmonth: date.getMonth(),
+          clickyear: date.getFullYear(),
+          userid: data.user_id,
+        });
+      } else {
+        Alert.alert(`Failed to save diary: ${data.error}`);
+      }
+    } catch (error) {
+      console.error('Error saving diary data:', error);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      const response = await apiClient.delete('/diary/delete-diary', {
+        data: { id: diaryId },
+      });
+      console.log("API Response:", response); // 추가된 로그
+      const data = response.data; // response.data를 직접 가져오기
+      if (data) {
+        navigation.navigate('Main');
+      } else {
+        Alert.alert(`Failed to delete diary: ${data.error}`);
+      }
+    } catch (error) {
+      console.error('Error deleting diary data:', error);
+    }
+  };
+  
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+          {/* <Text style={styles.Text} onPress={handlePrevDate}>◀</Text> */}
+          <Text style={styles.dateText}>{formatDate(date)}</Text>
+          {/* <Text style={styles.Text} onPress={handleNextDate}>▶</Text> */}
+      </View>
+      <View style={{marginBottom: 15,
+        padding: 10,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        shadowColor: '#FF5C85',
+        shadowOffset: {
+        width: 8,
+        height: 8,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 10,
+        elevation: 5,}}>
+        <TextInput
+          style={styles.inputHeadline}
+          placeholder="Headline"
+          value={headline} // Input 값 초기화
+          onChangeText={setHeadline}
+        />
+        <TextInput
+          style={styles.inputContent}
+          placeholder="Start typing..."
+          value={content} // Input 값 초기화
+          onChangeText={setContent}
+          multiline
+        />
+      </View>
+
+      <View style={styles.moodContainer}>
+        {['😞', '😠', '😐', '😊', '😄'].map((emoji, index) => (
+          <TouchableOpacity
+            key={index}
+            style={[styles.moodButton, mood === emoji && styles.moodButtonSelected]}
+            onPress={() => setMood(emoji)}
+          >
+            <Text style={styles.moodText}>{emoji}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+
+      <View style={styles.privacyContainer}>
+        {['Private', 'Couple'].map((option) => (
+          <TouchableOpacity
+            key={option}
+            style={[
+              styles.privacyOption,
+              styles.boxShadow,
+              privacy === option && styles.privacyOptionSelected // 조건부 스타일링
+            ]}
+            onPress={() => setPrivacy(option as 'Private' | 'Couple')}
+          >
+            <Text style={[styles.privacyText, privacy === option && styles.privacyTextSelected]}>
+              {option}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+
+      <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
+        <Text style={styles.saveButtonText}>Save</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.deleteButton} onPress={handleDelete}>
+        <Text style={styles.deleteButtonText}>Delete</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      width: 393,
+      height: 852,
+      backgroundColor: 'white',
+      // borderRadius: 30,
+      // borderTopRightRadius: 0,
+      // borderBottomRightRadius: 0,
+      padding: 20,
+      alignSelf: 'center',
+      justifyContent: 'flex-start',
+      gap: 0,
+  },
+  header: {
+      width: '100%',
+      height: 57,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      paddingHorizontal: 10,
+      marginBottom: 20,
+      shadowColor: '#FAC6DC80',
+      shadowOffset: { width: 5, height: 8 },
+      shadowOpacity: 1,
+      shadowRadius: 20,
+      elevation: 10,
+  },
+  Text: {
+      fontFamily: 'Manrope',
+      fontSize: 17,
+      lineHeight: 24,
+  },
+  dateText: {
+      fontSize: 18,
+      fontWeight: 'bold',
+  },
+  questionContainer: {
+      height : "20%",
+      justifyContent: 'center',
+      marginBottom: 15,
+      padding: 10,
+      backgroundColor: 'white',
+      borderRadius: 20,
+      shadowColor: '#FF5C85',
+      shadowOffset: {
+      width: 8,
+      height: 8,
+      },
+      shadowOpacity: 0.2,
+      shadowRadius: 10,
+      elevation: 8,
+  },
+  questionText: {
+      fontSize: 16,
+      fontWeight: 'bold',
+  },
+  nextButton: {
+      backgroundColor: '#F6A5C0',
+      padding: 12,
+      borderRadius: 8,
+      alignItems: 'center',
+      marginTop: 20,
+      marginBottom: 15,
+      shadowColor: '#FF5C85',
+      shadowOffset: {
+      width: 8,
+      height: 8,
+      },
+      shadowOpacity: 0.2,
+      shadowRadius: 10,
+      elevation: 10,
+  },
+  nextButtonText: {
+      color: '#FFF',
+      fontSize: 16,
+      fontWeight: 'bold',
+  },
+  inputHeadline: {
+      backgroundColor: '#FFF',
+      padding: 12,
+      borderRadius: 8,
+      fontSize: 16,
+      borderBottomWidth: 0.5,
+      borderBottomColor : 'lightgray'
+  },
+  inputContent: {
+      backgroundColor: '#FFF',
+      padding: 12,
+      borderRadius: 8,
+      fontSize: 16,
+      minHeight: 300,
+      textAlignVertical: 'top',
+      borderTopWidth : 1,
+      borderTopColor: 'gray',
+      marginBottom: 16,
+  },
+  moodContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      marginBottom: 20,
+      padding: 10,
+      backgroundColor: 'white',
+      borderRadius: 20,
+      shadowColor: '#FF5C85',
+      shadowOffset: {
+      width: 8,
+      height: 8,
+      },
+      shadowOpacity: 0.2,
+      shadowRadius: 10,
+      elevation: 5,
+  },
+  moodButton: {
+      padding: 8,
+      borderRadius: 8,
+      backgroundColor: '#FFF',
+      shadowColor: '#FAC6DC80',
+      shadowOffset: { width: 5, height: 8 },
+      shadowOpacity: 1,
+      shadowRadius: 20,
+      elevation: 10,
+      borderWidth: 1,
+      borderColor: '#FAC6DC',
+      marginHorizontal: 8,
+  },
+  moodButtonSelected: {
+      backgroundColor: '#FAC6DC',
+  },
+  moodText: {
+      fontSize: 24,
+  },
+  privacyContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      marginBottom: 20,
+      padding: 10,
+      backgroundColor: 'white',
+      borderRadius: 20,
+      shadowColor: '#FF5C85',
+      shadowOffset: {
+      width: 8,
+      height: 8,
+      },
+      shadowOpacity: 0.2,
+      shadowRadius: 10,
+      elevation: 5,
+  },
+  privacyOption: {
+      padding: 8,
+      borderRadius: 8,
+      backgroundColor: '#FFF',
+      borderWidth: 1,
+      borderColor: '#FAC6DC',
+      marginHorizontal: 8,
+  },
+  privacyOptionSelected: {
+      backgroundColor: '#FAC6DC',
+  },
+  privacyText: {
+      fontSize: 16,
+      color: '#555',
+  },
+  privacyTextSelected: {
+      color: '#000',
+      fontWeight: 'bold',
+  },
+  saveButton: {
+      backgroundColor: '#F6A5C0',
+      padding: 12,
+      borderRadius: 8,
+      alignItems: 'center',
+      marginTop: 10,
+      marginBottom: 15,
+      shadowColor: '#FF5C85',
+      shadowOffset: {
+      width: 8,
+      height: 8,
+      },
+      shadowOpacity: 0.2,
+      shadowRadius: 10,
+      elevation: 5,
+  },
+  saveButtonText: {
+      color: '#FFF',
+      fontSize: 16,
+      fontWeight: 'bold',
+  },
+  deleteButton: {
+    backgroundColor: 'white',
+    opacity: 0.7,
+    padding: 12,
+    borderRadius: 8,
+    borderColor:'red',
+    borderWidth: 1,
+    alignItems: 'center',
+    marginBottom: 15,
+    shadowColor: '#FF5C85',
+    shadowOffset: {
+    width: 8,
+    height: 8,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  deleteButtonText: {
+      color: 'red',
+      fontSize: 16,
+      fontWeight: 'bold',
+  },
+  reviewContainer: {
+      backgroundColor: '#FFF',
+      padding: 20,
+      borderRadius: 10,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 1 },
+      shadowOpacity: 0.1,
+      shadowRadius: 2,
+      elevation: 2,
+      marginBottom: 20,
+  },
+  reviewDate: {
+      fontSize: 16,
+      color: '#888',
+      marginBottom: 10,
+  },
+  reviewMood: {
+      fontSize: 24,
+      marginBottom: 10,
+  },
+  reviewHeadline: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 10,
+  },
+  reviewContent: {
+      fontSize: 16,
+      marginBottom: 10,
+  },
+  reviewPrivacy: {
+      fontSize: 14,
+      color: '#888',
+  },
+  boxShadow: {
+      shadowColor: '#FAC6DC80',
+      shadowOffset: { width: 5, height: 8 },
+      shadowOpacity: 1,
+      shadowRadius: 20,
+      elevation: 10,
+  },
+});
+
+export default EditDiaryScreen;
